78918a1d2600492bb2df3b0fb6aa905c3fd17845
Nate West <natescott.west@gmail.com>
2014-07-09 20:47:47 -0700

    First pass... Non-functioning

--- rsense-mode.el
+++ rsense-mode.el
@@ -4,11 +4,39 @@
 (defvar rsense-executable
   (executable-find "rsense"))
 
+(defvar rsense-client-executable
+  (executable-find "_rsense_commandline.rb"))
+
 (defvar cwd
   (file-name-directory (or load-file-name buffer-file-name)))
 
 (defvar rsense-start-command
   (concat rsense-executable " start --path " cwd))
 
+(defvar current-position
+  (format "%s:%s" (line-number-at-pos) (current-column)))
+
+(defvar autocomplete-command
+  (concat rsense-client-executable
+          " --project=" cwd
+          " --filepath=" buffer-file-name
+          " --text=" (buffer-string)
+          " --location=" current-position))
+
 (defun start-rsense ()
-  (shell-command rsense-start-command))
+  (call-process-shell-command rsense-start-command))
+
+;; This doesn't actually autocomplete yet...
+;; It just invokes the command line client.
+(defun rsense-complete
+  (shell-command autocomplete-command))
+
+;; Can't get the keymap quite right :( Don't want to replace all tabs...
+;; just when the cursor is on non-whitespace...
+(define-minor-mode rsense-mode
+  "RSense sees all."
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "TAB") 'rsense-complete)
+            map))
+
+(provide 'rsense-mode)
